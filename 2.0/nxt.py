97d757a535c17641761c4773022dd4342fbe37bf
prudviraj12 <prudvirajreddy98@gmail.com>
2026-01-27 22:32:23 +0530

    second

--- 2.0/nxt.py
+++ 2.0/nxt.py
@@ -26,4 +26,45 @@
 # heck
 # p
 # if a number is a multiple of 7 or not.
-n=int(input("enter a number:"))
+# n=int(input("enter a number:"))
+# if(n%7==0):
+#     print(n,"is a multiplle")
+# else:
+#     print(n,"is not a multiple")
+# lists and its some appli
+# red=[1,2,3,4,5]
+# # red[1:3]
+# print(red)
+# # red.
+# red.sort(reverse=True)
+# print(red)
+# red.insert(0,7)
+# print(red)
+# WAP to ask the user to enter names of their 3 favorite movies & store them in a list
+# stu=["ranam","rangam","raju"]
+# print(stu)
+# WAP to check if a list contains a palindrome of elements. (Hint: use copy( ) method)
+# [1, 2, 3, 2, 1] [1,
+# “abc”
+# ,
+# “abc”
+# , 1]
+# stu=["reddy","reddy"]
+# if(stu.reverse==stu):
+#     print("palindrome")
+# else:
+#     print("not a palindrome")
+# stu=["red","red"]
+# rev=stu.copy()
+# rev.reverse()
+# if(stu==rev):
+#     print("palindrome")
+# else:
+#     print("not a palindrome")
+# tup=("C","A","B","A","K","D","C","C")
+# tup.count("C")
+# print(tup.count("C"))
+# tup=["C","A","B","A","K","D","C","C"]
+# tup.sort()
+# print(tup)
+
