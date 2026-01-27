7d857d4dad6e02cca99553bc4bbc52d29d7fdcdc
prudviraj12 <prudvirajreddy98@gmail.com>
2026-01-27 22:01:29 +0530

    first commit

---
+++ 2.0/nxt.py
@@ -0,0 +1,29 @@
+# # print("hi")
+# # print("hello")
+# # print("hey")
+# # def greet():
+# #     print("greetings!") 
+# # greet()
+# # WAP to check if a number entered by the user is odd or even.
+
+# num = int(input("Enter a number: "))
+# if num %2 == 0:
+#     print(num,"is even number")
+# else:
+#     print(num,"is odd")
+# WAP to find the greatest of 3 numbers entered by the user
+# a=int(input("num1:"))
+# b=int(input("num2:"))
+# c=int(input("num3:"))
+# if(a>b and a>c):
+#     print(a,"is higheset")
+# elif(b>a and b>c):
+#     print(b,"is highest")
+# else:
+#     print(c,"is highest")
+# # WAP to c
+# # A
+# heck
+# p
+# if a number is a multiple of 7 or not.
+n=int(input("enter a number:"))
