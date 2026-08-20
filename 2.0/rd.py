c4c4ff8b2278dcb97945ba6cbc6d9cd5fa068bdb
prudviraj12 <prudvirajreddy98@gmail.com>
2026-08-20 22:31:34 +0530

    distribution of elements into arrays

--- 2.0/rd.py
+++ 2.0/rd.py
@@ -14,9 +14,25 @@
 #     else:
 #         print("Not a prime number")n = int(input("Enter the number of terms: "))
 
-a, b = 0, 1
+# a, b = 0, 1
 
-for i in range(n):
-    print(a, end=" ")
-    a, b = b, a + b
+# for i in range(n):
+#     print(a, end=" ")
+#     a, b = b, a + b
     
+class Solution:
+    def resultArray(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        arr1 = []
+        arr2 = []
+
+        arr1.append(nums[0])
+        arr2.append(nums[1])
+
+        for i in range(2, n):
+            if arr1[-1] > arr2[-1]:
+                arr1.append(nums[i])
+            else:
+                arr2.append(nums[i])
+        
+        return arr1 + arr2
