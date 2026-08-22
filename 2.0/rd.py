79c4afd3885cce835a27ee240ff0bc3566f857e9
prudviraj12 <prudvirajreddy98@gmail.com>
2026-08-22 22:51:45 +0530

    code

--- 2.0/rd.py
+++ 2.0/rd.py
@@ -20,19 +20,33 @@
 #     print(a, end=" ")
 #     a, b = b, a + b
     
-class Solution:
-    def resultArray(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        arr1 = []
-        arr2 = []
+# class Solution:
+#     def resultArray(self, nums: List[int]) -> List[int]:
+#         n = len(nums)
+#         arr1 = []
+#         arr2 = []
 
-        arr1.append(nums[0])
-        arr2.append(nums[1])
+#         arr1.append(nums[0])
+#         arr2.append(nums[1])
 
-        for i in range(2, n):
-            if arr1[-1] > arr2[-1]:
-                arr1.append(nums[i])
-            else:
-                arr2.append(nums[i])
+#         for i in range(2, n):
+#             if arr1[-1] > arr2[-1]:
+#                 arr1.append(nums[i])
+#             else:
+#                 arr2.append(nums[i])
         
-        return arr1 + arr2
+#         return arr1 + arr2n = int(input("Enter a number: "))
+
+square = n * n
+sum_digits = 0
+
+while square > 0:
+    digit = square % 10
+    sum_digits += digit
+    square //= 10
+
+if sum_digits == n:
+    print(n, "is a Neon Number")
+else:
+    print(n, "is not a Neon Number")
+    
